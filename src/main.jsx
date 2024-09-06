e084d8013c68f0480857c5ae235a54d56a8157e3
Mintu Sikder <mintusikder15@gmail.com>
2024-09-06 19:35:43 +0600

    added navebar section

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './layout/Main';
+import Home from './components/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>,
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+ <RouterProvider router={router} />
+  </StrictMode>,
+)
