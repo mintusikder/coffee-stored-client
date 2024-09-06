d6172c13bc44f3335fb29f8ad9106be62e0013cc
Mintu Sikder <mintusikder15@gmail.com>
2024-09-06 22:55:21 +0600

    added coffee card

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,13 +1,38 @@
 import { MdDelete, MdEdit } from "react-icons/md";
 import { FaEye } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const {_id, chef, name, supplier, taste, category, details, url } = coffee;
+  const { _id, chef, name, supplier, taste, category, details, url } = coffee;
 
-  const handelDelete = () =>{
-    
-  }
+  const handelDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="card card-side bg-base-100 shadow-xl p-6">
@@ -25,19 +50,13 @@ const CoffeeCard = ({ coffee }) => {
         </div>
         <div className="card-actions justify-end flex-col">
           <button className="btn btn-primary">
-            <Link to={`/coffee/${coffee.id}`}>
-              <FaEye />
-            </Link>
+            <FaEye />
           </button>
           <button className="btn btn-neutral">
-            <Link to={`/edit/${coffee.id}`}>
-              <MdEdit />
-            </Link>
+            <MdEdit />
           </button>
-          <button className="btn btn-error">
-            <Link onClick={handelDelete}>
-              <MdDelete />
-            </Link>
+          <button onClick={() => handelDelete(_id)} className="btn btn-error">
+            <MdDelete />
           </button>
         </div>
       </div>
