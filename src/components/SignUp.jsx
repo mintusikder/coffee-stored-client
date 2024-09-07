47662f8a57a09169337a04b7086030ce8e928763
Mintu Sikder <mintusikder15@gmail.com>
2024-09-07 09:23:38 +0600

    added login and registration page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,70 @@
-
 const SignUp = () => {
-    return (
-        <div>
-            
+  const handelReg = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const loginUser = { name, email, password };
+    console.log(loginUser);
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Registration now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handelReg} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Registration</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,19 +1,26 @@
 import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
-export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
 
-const AuthProvider = ({children}) => {
-    const [ user, setUser] = useState(null)
-    const [loader, setLoader] = useState(true)
-    const info ={
-        loader,
-        user
-    }
-    return (
-        <AuthContext.Provider value={info}>
-            {children}
-        </AuthContext.Provider>
-    );
+  //create user
+  const createUser = (email, password) => {
+    setLoader(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const info = {
+    loader,
+    user,
+    createUser,
+  };
+
+  return <AuthContext.Provider value={info}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
