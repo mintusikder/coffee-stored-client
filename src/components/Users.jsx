fd28c8a57c6a9e8423baeaf1d8a4fb6405c75d0d
Mintu Sikder <mintusikder15@gmail.com>
2024-09-07 22:53:19 +0600

    added user delete

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,61 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
-
 const Users = () => {
-    const loadedData = useLoaderData()
-    return (
-        <div>
-            <h2>Users: {loadedData.length}</h2>
-        </div>
-    );
+  const loadedUser = useLoaderData();
+  const [users, setUsers] = useState(loadedUser);
+  const handelDelete = (id) => {
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.deletedCount > 0){
+          alert("Delete successful")
+        }
+        const remaining = users.filter(user => user._id !== id)
+        setUsers(remaining)
+      });
+  };
+  return (
+    <div>
+      <h2>Users: {users.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Email</th>
+              <th>Created At</th>
+              <th>Last Login</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {users.map((user) => (
+              <tr key={user._id}>
+                <th>1</th>
+                <td>{user.email}</td>
+                <td>{user.createdAt}</td>
+                <td>Blue</td>
+                <td>
+                  <button
+                    onClick={() => handelDelete(user._id)}
+                    className="btn"
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
